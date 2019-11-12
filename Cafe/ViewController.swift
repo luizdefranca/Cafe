f93a556d1e8beba10708309e454e5d4a8cc8e4f6
LuizRamos <luizramospe@hotmail.com>
2019-11-11 22:16:39 -0500

    added search method for searchBar and setup the layout of navigationBar

--- Cafe/ViewController.swift
+++ Cafe/ViewController.swift
@@ -19,19 +19,16 @@ class ViewController: UIViewController {
 
     var cafes: [Cafe] = []
     var searchResults: [Cafe] = []
-    var searchController: UISearchController  {
-        let searchController  = UISearchController(searchResultsController: nil)
-        searchController.obscuresBackgroundDuringPresentation = false
-        return searchController
-    }
+    var searchController: UISearchController!
 
 
     //MARK: Constants
-     let cellIdentifier = "cell"
+    let cellIdentifier = "cell"
 
     //MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+
         listTableView.backgroundColor = UIColor(named: "Destak_Background")
         listTableView.dataSource = self
         listTableView.delegate = self
@@ -62,24 +59,12 @@ class ViewController: UIViewController {
 
     }
 
-    private func configTableView() {
-        listTableView.estimatedRowHeight = 80
-        listTableView.rowHeight = UITableView.automaticDimension
-    }
-
-    private func animations() {
-        let anin = UIViewPropertyAnimator()
-        
-        anin.addAnimations {
-
-        }
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case Segue.showDetail.rawValue:
             if let indexPath = listTableView.indexPathForSelectedRow {
                 if let destination = segue.destination as? DetailTableViewController {
-                    destination.cafe = cafes[indexPath.row]
+                    destination.cafe = (searchController.isActive) ? searchResults[indexPath.row] : cafes[indexPath.row]
                     let cell = listTableView.cellForRow(at: indexPath) as! CafeTableViewCell
                     destination.image = cell.thumbnailImage.image
                 }
@@ -90,7 +75,20 @@ class ViewController: UIViewController {
     }
 
     //MARK: - Functions
-
+    func filterContent(for searchText: String) {
+        self.searchResults = cafes.filter({ (cafe) -> Bool in
+            if searchText.trimmingCharacters(in: CharacterSet(charactersIn: "")) != "" || searchText.count == 0 {
+                if cafe.name.range(of: searchText) != nil || cafe.address.range(of: searchText) != nil {
+                               print("true")
+                               return true
+                } else {
+                    return false
+                }
+                           print("false")
+            }
+            return true
+        })
+    }
     func fetchData(){
         let cafeManager = CafeManager()
         self.cafes = cafeManager.cafes
@@ -104,15 +102,20 @@ class ViewController: UIViewController {
 //MARK: TableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cafes.count
+
+        if searchController.isActive {
+            return searchResults.count
+        } else {
+            return cafes.count
+        }
+
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CafeTableViewCell
-        let cafe = cafes[indexPath.row]
+        let cafe = (searchController.isActive) ? searchResults[indexPath.row] : cafes[indexPath.row]
         cell.cafe = cafe
-        //        cell.nameLabel.text = self.restaurantNames[indexPath.row]
-//        cell.thumbnailImage.image = UIImage(named: "\(self.restaurantNames[indexPath.row])")
+
         let backgroundView = UIView()
         backgroundView.backgroundColor = UIColor(named: "Destak_Background")
 
@@ -130,7 +133,7 @@ extension ViewController: UITableViewDelegate {
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         optionMenu.addAction(cancelAction)
         optionMenu.view.tintColor = UIColor(named: "Dark_Font")
-//        present(optionMenu, animated: true, completion: nil)
+        //        present(optionMenu, animated: true, completion: nil)
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -142,32 +145,19 @@ extension ViewController: UITableViewDelegate {
             //TODO: implement
             print("Todo")
         }
+
+
+
     }
 
-    /*
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        //Sharing Button
-        let shareAction = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) -> Void in
-            let defaultText = "Checking in at " + self.restaurantNames[indexPath.row]
-            let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
-            self.present(activityController, animated: true, completion: {
-
-            })
-
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if searchController.isActive {
+            return false
+        } else {
+            return true
         }
-
-        shareAction.backgroundColor = UIColor(displayP3Red: 48.0/255.0, green: 173.0/255.0, blue: 99.0/255.0, alpha: 1.0)
-
-
-        //Delete button
-        let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) -> Void in
-            self.restaurantNames.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-        deleteAction.backgroundColor = UIColor(displayP3Red: 202.0/255.0, green: 202.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        return [deleteAction, shareAction]
     }
-*/
+
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
 
         let deleteButton = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
@@ -202,39 +192,121 @@ extension ViewController: UITableViewDelegate {
     }
 
 }
+//MARK: - NSFetchedREsulsControllerDelegate
+extension ViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        print("\(#function) - \(#line)")
+        if let searchText = searchController.searchBar.text {
+            filterContent(for: searchText)
+            listTableView.reloadData()
+            print("\(#function) - \(#line)")
+        }
+    }
+
+
+}
+
+
 
 //MARK: - View Methods
 
 //MARK: Search Bar and Search Controller
 
 extension ViewController {
-    private func addSearchBar(){
 
-        let searchBar = searchController.searchBar
+
+    private func configTableView() {
+        listTableView.estimatedRowHeight = 80
+        listTableView.rowHeight = UITableView.automaticDimension
+    }
+
+    private func animations() {
+        let anin = UIViewPropertyAnimator()
+
+        anin.addAnimations {
+
+        }
+    }
+    private func addSearchBar(){
+        searchController  = UISearchController(searchResultsController: nil)
+
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        var searchBar = searchController.searchBar
         searchBar.enablesReturnKeyAutomatically = true
         searchBar.isTranslucent = true
 
-       // searchBar.setImage(UIImage(named: "search"), for: .search, state: .normal)
+        // searchBar.setImage(UIImage(named: "search"), for: .search, state: .normal)
+        searchBar.placeholder = "Search cafes..."
         searchBar.barTintColor = UIColor(named: "Dark_Font")
         searchBar.backgroundColor = UIColor(named: "Clear_Light_Font")
         searchBar.tintColor = UIColor(named: "Clear_Light_Font")
         searchBar.barStyle = .blackOpaque
-        if #available(iOS 11, *){
+        searchBar.showsCancelButton = true
+
+//        if #available(iOS 11, *){
             if let textField = searchBar.value(forKey: "searchField") as? UITextField {
                 textField.textColor = UIColor(named: "Clear_Light_Font")
                 textField.font = UIFont(name: "ArialRoundedMTBold", size: 17)
                 let iconView = textField.leftView as! UIImageView
                 iconView.image = iconView.image?.withRenderingMode(.alwaysTemplate)
                 iconView.tintColor = UIColor(named: "Clear_Light_Font")
-//                if let backgroundView = textField.subviews.first {
-//
-//                    backgroundView.backgroundColor = UIColor(named: "Destak_Background")
-//                }
+                //                if let backgroundView = textField.subviews.first {
+                //
+                //                    backgroundView.backgroundColor = UIColor(named: "Destak_Background")
+                //                }
             }
-        }
-        listTableView.tableHeaderView = searchBar
-//        listTableView.tableHeaderView?.
-//        listTableView.backgroundColor = UIColor(named: "Dark_Font")
+//        }
+//navigationItem.searchController = searchController
+
+                listTableView.tableHeaderView = searchBar
+        //        listTableView.tableHeaderView?.
+        //        listTableView.backgroundColor = UIColor(named: "Dark_Font")
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+ //Sharing Button
+ let shareAction = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) -> Void in
+ let defaultText = "Checking in at " + self.restaurantNames[indexPath.row]
+ let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+ self.present(activityController, animated: true, completion: {
+
+ })
+
+ }
+
+ shareAction.backgroundColor = UIColor(displayP3Red: 48.0/255.0, green: 173.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+
+
+ //Delete button
+ let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) -> Void in
+ self.restaurantNames.remove(at: indexPath.row)
+ tableView.deleteRows(at: [indexPath], with: .fade)
+ }
+ deleteAction.backgroundColor = UIColor(displayP3Red: 202.0/255.0, green: 202.0/255.0, blue: 203.0/255.0, alpha: 1.0)
+ return [deleteAction, shareAction]
+ }
+ */
